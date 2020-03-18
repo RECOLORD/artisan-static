f9c1eb12456e2d72feb162b73ac7da78e8893ce1
RECOLORD <36219497+RECOLORD@users.noreply.github.com>
2020-03-18 22:36:44 +0900

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -6,6 +6,4 @@
     <h2>Asset Compilation Test</h2>
 
     <p>CSS test: <span class="test-css">text with border and padding</span></p>
-
-    <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
 @endsection
